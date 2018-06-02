5da89c0b9ecaa33481ada933677a21d7fb6241c5
hectwor <hectorh.danielv@gmail.com>
2018-06-02 08:59:38 -0500

    Add Access-Control-Allow-Origin

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+let bodyParser = require('body-parser');
 const router = express.Router();
 const algoritms = require('../src/algoritms');
+
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+router.use(bodyParser.json());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
